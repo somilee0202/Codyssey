c428ebd9b05e2595fbbfe16dd6bd4c7ba9dfe697
이 소민 <somin990202@c6r5s8.codyssey.kr>
2025-07-14 17:25:18 +0900

    app.py

---
+++ 과제_1/문제2/app.py
@@ -0,0 +1,9 @@
+from flask import Flask 
+app = Flask(__name__) #Flask 앱 생성 (앱 이름)
+
+@app.route("/") # 라우팅: "/" 주소 요청시 아래 함수 실행
+def hello_world():
+    return "Hello, DevOps!"
+
+if __name__ == "__main__": # 이 파일을 직접 실행한 경우에만 서버 실행
+    app.run(host="0.0.0.0", port=8080) # 0.0.0.0 ->와부기기 접속 허용, 8080포트로 연결
