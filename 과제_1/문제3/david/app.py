82d95bd73337b02517c60601ccff4c5e410a25a2
이 소민 <somin990202@c6r5s8.codyssey.kr>
2025-07-15 17:08:34 +0900

    문제3

--- 과제_1/문제3/david/app.py
+++ 과제_1/문제3/david/app.py
@@ -3,7 +3,7 @@ import os
 from io import BytesIO
 from gtts import gTTS
 
-DEFAULT_LANG = os.getenv('DEFAULT_LANG', 'ko')
+DEFAULT_LANG = os.getenv('DEFAULT_LANG', 'en')
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,9 +13,9 @@ def home():
 
     lang = request.args.get('lang', DEFAULT_LANG)
     fp = BytesIO()
-    gTTS(text, "com", lang).write_to_fp(fp)
+    gTTS(text, tld="com", lang=lang).write_to_fp(fp)
 
     return Response(fp.getvalue(), mimetype='audio/mpeg') # 페이지 전달없이 바로 재생
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 80)
+    app.run('0.0.0.0', 8080, debug=True)
