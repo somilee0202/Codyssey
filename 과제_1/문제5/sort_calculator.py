f2592f7fa8d436ed4a596f7f9d2b3d42feb83f8c
Somin <somin990202@naver.com>
2025-07-18 12:02:47 +0900

    first commit

---
+++ 과제_1/문제5/sort_calculator.py
@@ -0,0 +1,24 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def main():
+    try:
+        arr = list(map(float, input("Enter numbers: ").split()))
+        if not arr:
+            print("Invalid input.")
+            return
+    except:
+        print("Invalid input.")
+        return
+
+    bubble_sort(arr)
+    sorted = ' '.join(f"<{num}>" for num in arr)
+    print(f"Sorted: {sorted}")
+
+if __name__ == "__main__":
+    main()
