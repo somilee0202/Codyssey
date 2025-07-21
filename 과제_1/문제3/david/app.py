fb5f0d629246b4eff227502ce9d59b6e8f8d77a7
Somin <somin990202@naver.com>
2025-07-21 20:30:57 +0900

    ss

--- 과제_1/문제3/david/app.py
+++ 과제_1/문제3/david/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, Response
 import os
-from io import BytesIO
+from io import BytesIO # mp3 파일을 메모리에 저장(파일 X)
 from gtts import gTTS
 
 DEFAULT_LANG = os.getenv('DEFAULT_LANG', 'en')
@@ -8,14 +8,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
     text = "Hello, DevOps"
 
     lang = request.args.get('lang', DEFAULT_LANG)
-    fp = BytesIO()
-    gTTS(text, tld="com", lang=lang).write_to_fp(fp)
+    fp = BytesIO() # mp3 파일을 저장할 객체
+    gTTS(text, tld="com", lang=lang).write_to_fp(fp) # mp3 데이터를 fp에 저장
 
-    return Response(fp.getvalue(), mimetype='audio/mpeg') # 페이지 전달없이 바로 재생
+    return Response(fp.getvalue(), mimetype='audio/mpeg')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8080, debug=True)
--- 과제_1/문제3/david/calculator_bonus.py
+++ 과제_1/문제3/david/calculator_bonus.py
@@ -1,9 +1,47 @@
+def add(a, b):
+    print(f"Result: <{a + b}>")
+    return
+
+def sub(a, b):
+    print(f"Result: <{a - b}>")
+    return
+
+def mul(a, b):
+    print(f"Result: <{a * b}>")
+    return
+
+def div(a, b):
+    if b == 0:
+        print("Error: Division by zero.")
+    else:
+        print(f"Result: <{a / b}>")
+    return
+
 def cal():
     try:
         a, operator, b = input("Enter expression: ").split(" ")
-        print(f"a= {a}, operator= {operator}, b= {b}")
     except ValueError as e:
         print(f"Error: {e}")
+        return
+
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        print("Invalid number input.")
+        return
+
+    if operator == "+":
+        add(a, b)
+    elif operator == "-":
+        sub(a, b)
+    elif operator == "*":
+        mul(a, b)
+    elif operator == "/":
+        div(a, b)
+
+    else:
+        print("Invalid operator." )
 
 if __name__ == "__main__":
     cal()
